bf7cfb6791e0135c20b4ed991e5eae87b80f141d
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-02-25 22:47:34 +0530

    Create SumofArray.cpp

---
+++ Day5/SumofArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    long long sum = 0, x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        sum += x;
+    }
+
+    cout << sum;
+    return 0;
+}
