3716bbbcf0e790a415f042d1b07537522b69cab0
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-02-27 21:58:44 +0530

    Create ColumnOrder.cpp

---
+++ Day6/ColumnOrder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int a[1000][1000];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+
+    for (int j = 0; j < m; j++)
+        for (int i = 0; i < n; i++)
+            cout << a[i][j] << " ";
+}
