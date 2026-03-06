cc6562a909348cdff2241dfb4c9c33fcf90e00b8
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-06 23:52:10 +0530

    Add LexicographicalOrder.cpp for string comparison

---
+++ Day7/LexicographicalOrder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string a,b;
+    cin>>a>>b;
+
+    if(a<b) cout<<"A";
+    else if(b<a) cout<<"B";
+    else cout<<"Equal";
+}
