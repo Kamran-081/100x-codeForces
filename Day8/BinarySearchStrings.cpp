44995432c8b3e0b98aeeafbcf0820bd17a024900
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-07 23:48:31 +0530

    Create BinarySearchStrings.cpp

---
+++ Day8/BinarySearchStrings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    string a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    string x;
+    cin>>x;
+
+    int l=0,r=n-1;
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(a[mid]==x){
+            cout<<"YES";
+            return 0;
+        }
+        else if(a[mid]<x) l=mid+1;
+        else r=mid-1;
+    }
+
+    cout<<"NO";
+}
