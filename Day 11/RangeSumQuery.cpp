f67fdcf3d996e215adb3620c993415a7b4a1f1a7
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-13 23:25:41 +0530

    Create RangeSumQuery.cpp

---
+++ Day 11/RangeSumQuery.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    long long a[n+1], prefix[n+1];
+
+    for(int i=1;i<=n;i++)
+        cin >> a[i];
+
+    prefix[0] = 0;
+
+    for(int i=1;i<=n;i++)
+        prefix[i] = prefix[i-1] + a[i];
+
+    int q;
+    cin >> q;
+
+    while(q--){
+        int l,r;
+        cin >> l >> r;
+
+        cout << prefix[r] - prefix[l-1] << endl;
+    }
+}
