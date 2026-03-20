537b5ed3bafc39f1ff25fbd1ecf58abf7386f55f
Kamran Nazir <SheikhKamran1845@gmail.com>
2026-03-20 22:43:11 +0530

    Create CountDistinct.cpp

---
+++ Day12/CountDistinct.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<long long> s;
+    for(int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+        s.insert(x);
+    }
+
+    cout << s.size();
+}
